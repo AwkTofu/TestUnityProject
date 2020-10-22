a8f781fd4dfeef2a369411279759ff38e134c268
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-10-22 19:43:37 -0400

    Fixed Error with prefab

---
+++ Assets/Script/DataTransfer/ShowTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTime : MonoBehaviour
+{
+
+    public Text timeTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeTxt.text = "Time: " + StaticVar.num;
+    }
+}
