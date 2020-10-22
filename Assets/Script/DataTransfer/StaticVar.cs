0251b97009b065e02c38056e40cd46b4e79280ce
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-10-22 19:34:45 -0400

    public static vars keeps data between scene

---
+++ Assets/Script/DataTransfer/StaticVar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float num= 2;
+    public static string txt = "Hello World";
+
+    void Update()
+    {
+        num += (Time.deltaTime * 1.5f);   
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log(num);
+        }
+    }
+}
