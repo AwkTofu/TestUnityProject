7ef91f0a2454251438565b7221381bc5581cae8d
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-10-22 17:32:07 -0400

    Testing with DontDestroyOnLoad

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private int randomNumber = Random.Range(0, 100);
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int getRandomInt()
+    {
+        return randomNumber;
+    }
+}
