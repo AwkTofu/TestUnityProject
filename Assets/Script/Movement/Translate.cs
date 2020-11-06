80b65625864134f62bbb5627690152392bf5219c
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-11-05 21:42:58 -0500

    finished movement

--- Assets/Script/Movement/Translate.cs
+++ Assets/Script/Movement/Translate.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class Translate : MonoBehaviour
 {
     public float speed = 10f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
