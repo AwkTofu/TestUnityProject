68bd992585db74816b2a05885bdd6d676b67b624
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-11-12 22:58:01 -0500

    Added Script for Sphere

--- Assets/Script/StartAwake/SphereObj.cs
+++ Assets/Script/StartAwake/SphereObj.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class SphereObj : MonoBehaviour
 {
+    void Awake()
+    {
+        Debug.Log("Sphere: Test in Awake ");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Sphere: Test in Start ");
     }
 
     // Update is called once per frame
