d6be02c12ba6acfa14765967eddca168ee2791bb
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-11-12 22:59:32 -0500

    Added Script for Cube

--- Assets/Script/StartAwake/SqrObj.cs
+++ Assets/Script/StartAwake/SqrObj.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class SqrObj : MonoBehaviour
 {
+    void Awake()
+    {
+        Debug.Log("Cube: Test in Awake ");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Cube: Test in Start ");
     }
+
 
     // Update is called once per frame
     void Update()
