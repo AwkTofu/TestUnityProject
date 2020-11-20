77b74999fbb99db67d3ae0bc2ac4b7db00269e79
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-11-19 21:43:12 -0500

    Testing with vars

--- Assets/Script/RaycastMouse/MouseDown.cs
+++ Assets/Script/RaycastMouse/MouseDown.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class MouseDown : MonoBehaviour
 {
+    public int test = 0;
     void OnMouseDown()
     {
         // Destroy the gameObject after clicking on it
         Debug.Log("Clicked on the item");
+        if (test == 0)
+        {
+            Debug.Log("Starting with 0");
+            test++;
+        }
+        else if (test == 1)
+        {
+            Debug.Log("Now at 1");
+            test++;
+        }
+        else if (test == 2)
+        {
+            Debug.Log("Time to reset to 0");
+            test = 0;
+        }
     }
 }
