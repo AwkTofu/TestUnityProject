bcf75e7aca6c7ee28fd0f58096d0a8ef9e0168ec
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-11-19 21:36:51 -0500

    Script for onMouseDown

---
+++ Assets/Script/RaycastMouse/MouseDown.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDown : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        // Destroy the gameObject after clicking on it
+        Debug.Log("Clicked on the item");
+    }
+}
